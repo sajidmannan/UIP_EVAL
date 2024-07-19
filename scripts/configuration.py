b0715780a328965a98155aa945081d0f17fc66db
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2024-07-19 14:13:03 -0700

    feat: added configuration example as a note
    Signed-off-by: Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>

---
+++ scripts/configuration.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+
+"""
+This script is not intended to work entirely, but
+shows the key elements needed for training (for 
+the Intel folks, "BKM").
+
+These are:
+
+- For energies, use ``AtomWeightedMSE`` as the metric. The new default behavior
+  for ``ForceRegressionTask`` uses this, but if you use another task to represent
+  it this may need to be manually set.
+- Use stochastic weight averaging callback
+- Use exponential moving average callback
+- Use gradient clipping
+
+An important thing to note about EMA is that the exponential weights
+are used _outside_ of training: for validation/testing/ASE usage,
+the EMA weights will be used instead of the "vanilla" model. This
+means that logged values are not necessarily directly comparable
+between training and validation, or rather, validation should be
+outperforming training.
+"""
+from pytorch_lightning.callbacks import StochasticWeightAveraging
+
+from matsciml.models.losses import AtomWeightedMSE
+from matsciml.lightning.callbacks import ExponentialMovingAverageCallback, ManualGradientClip
+
+
+# pass into trainer configuration
+callbacks = [
+    StochasticWeightAveraging(swa_lrs=1e-2, swa_epoch_start=1),
+    ExponentialMovingAverageCallback(decay=0.99),
+    ManualGradientClip(value=10., algorithm="norm")
+]
