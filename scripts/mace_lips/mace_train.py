36192ab085292e11a9e91101b5d3e7e042ce4aa7
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2024-07-24 10:02:26 -0700

    scripts: added training and validation scripts for MACE on LiPS
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

---
+++ scripts/mace_lips/mace_train.py
@@ -0,0 +1,119 @@
+import pytorch_lightning as pl
+import torch
+from e3nn.o3 import Irreps
+from mace.modules import ScaleShiftMACE
+from mace.modules.blocks import RealAgnosticResidualInteractionBlock
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import (
+    StochasticWeightAveraging,
+    LearningRateMonitor,
+    ModelCheckpoint,
+)
+from torch import nn
+
+from matsciml.datasets.transforms import (
+    PeriodicPropertiesTransform,
+    PointCloudToGraphTransform,
+)
+from matsciml.lightning.data_utils import MatSciMLDataModule
+fom matsciml.models.base import ForceRegressionTask
+from matsciml.models.pyg.mace import MACEWrapper
+from matsciml.lightning.callbacks import (
+    ExponentialMovingAverageCallback,
+    ManualGradientClip,
+)
+"""
+This script is used to reproduce the MACE training run on the
+full LiPS dataset using the matsciml pipeline.
+
+The run should be reproducible using public matsciml#940575c,
+and should be entirely self-contained.
+
+Notable things:
+    1. Uses a variety of callbacks, particularly gradient clipping
+    and exponential moving average weights.
+    2. Periodic boundary conditions with a cut off of 5
+    3. Logs to weights and biases
+    4. Sets medium precision for single precision; uses tensor cores at
+    lower precision (i.e. FP32 = combined FP16) but improves throughput
+"""
+
+pl.seed_everything(215125)
+# use tensor cores
+torch.set_float32_matmul_precision("medium")
+
+available_models = {
+    "mace": {
+        "encoder_class": MACEWrapper,
+        "encoder_kwargs": {
+            "mace_module": ScaleShiftMACE,
+            "num_atom_embedding": 100,  # this is set to 100 and will use ion energies
+            "r_max": 5.0,
+            "num_bessel": 8,
+            "num_polynomial_cutoff": 5.0,
+            "max_ell": 3,
+            "interaction_cls": RealAgnosticResidualInteractionBlock,
+            "interaction_cls_first": RealAgnosticResidualInteractionBlock,
+            "num_interactions": 2,
+            "hidden_irreps": Irreps("128x0e + 128x1o"),
+            "atom_embedding_dim": 16,
+            "MLP_irreps": Irreps("16x0e"),
+            "avg_num_neighbors": 25.188983917236328,
+            "correlation": 3,
+            "radial_type": "bessel",
+            "gate": nn.SiLU(),
+            "atomic_inter_scale": 0.610558,
+            "atomic_inter_shift": 0,
+            "distance_transform": None,
+        },
+        # note we are using the output heads for the task - not outputs from MACE!
+        "output_kwargs": {"lazy": False, "input_dim": 640, "hidden_dim": 640},
+        "task_loss_scaling": {"energy": 1, "force": 10},
+    }
+}
+
+ROOT_DIR = "/datasets-alt/molecular-data/lips"
+task = ForceRegressionTask(**available_models["mace"])
+
+dm = MatSciMLDataModule(
+    "LiPSDataset",
+    train_path=f"{ROOT_DIR}/train",
+    val_split=f"{ROOT_DIR}/val",
+    dset_kwargs={
+        "transforms": [
+            PeriodicPropertiesTransform(5.0, adaptive_cutoff=True),
+            PointCloudToGraphTransform(
+                "pyg",
+                node_keys=["pos", "atomic_numbers"],
+            ),
+        ],
+    },
+    batch_size=16,
+    num_workers=8,
+)
+
+save_dir = "./wandb_logs"
+wb_logger = WandbLogger(
+    log_model="all",
+    save_dir=save_dir,
+    project="matsciml-uip-eval",
+    entity="laserkelvin",
+    mode="online",
+)
+
+trainer = pl.Trainer(
+    accelerator="cuda",
+    devices=1,
+    max_epochs=100,
+    logger=wb_logger,
+    callbacks=[
+        StochasticWeightAveraging(
+            swa_lrs=1e-2, swa_epoch_start=0.6, annealing_epochs=30
+        ),
+        ExponentialMovingAverageCallback(decay=0.99),
+        ManualGradientClip(10.0),
+        LearningRateMonitor("step"),
+        ModelCheckpoint(monitor="val_force_epoch", save_top_k=5),
+    ],
+)
+trainer.fit(task, datamodule=dm)
---
+++ scripts/mace_lips/mace_validation.py
@@ -0,0 +1,113 @@
+from pathlib import Path
+
+import pytorch_lightning as pl
+import torch
+import wandb
+import numpy as np
+from e3nn.o3 import Irreps
+from mace.modules import ScaleShiftMACE
+from mace.modules.blocks import RealAgnosticResidualInteractionBlock
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import (
+    StochasticWeightAveraging,
+    LearningRateMonitor,
+    Callback,
+    ModelCheckpoint,
+)
+from torch.optim.swa_utils import AveragedModel, get_ema_multi_avg_fn
+from torch import nn
+
+from matsciml.common.types import AbstractGraph, BatchDict, DataDict
+from matsciml.datasets import LiPSDataset
+from matsciml.datasets.transforms import (
+    PeriodicPropertiesTransform,
+    PointCloudToGraphTransform,
+)
+from matsciml.lightning.data_utils import MatSciMLDataModule
+from matsciml.models.base import ForceRegressionTask
+from matsciml.models.pyg.mace import MACEWrapper
+from matsciml.lightning.callbacks import (
+    ExponentialMovingAverageCallback,
+    ManualGradientClip,
+)
+
+pl.seed_everything(215125)
+torch.set_float32_matmul_precision("medium")
+
+run = wandb.init(
+    project="matsciml-uip-eval", tags=["inference", "validation", "results"]
+)
+
+artifact = run.use_artifact(
+    "laserkelvin/matsciml-uip-eval/model-pfs05aqp:v74", type="model"
+)
+artifact_dir = Path(artifact.download())
+
+task = ForceRegressionTask.load_from_checkpoint(artifact_dir.joinpath("model.ckpt"))
+
+# move task to device
+task = task.to("cuda")
+
+ROOT_DIR = "/datasets-alt/molecular-data/lips"
+
+dm = MatSciMLDataModule(
+    "LiPSDataset",
+    train_path=f"{ROOT_DIR}/train",
+    val_split=f"{ROOT_DIR}/val",
+    dset_kwargs={
+        "transforms": [
+            PeriodicPropertiesTransform(5.0, adaptive_cutoff=True),
+            PointCloudToGraphTransform(
+                "pyg",
+                node_keys=["pos", "atomic_numbers"],
+            ),
+        ],
+    },
+    batch_size=16,
+    num_workers=8,
+)
+
+# manual inference
+dm.setup("fit")
+val_loader = dm.val_dataloader()
+
+
+def to(data, device):
+    new_dict = {}
+    for key, value in data.items():
+        if hasattr(value, "to"):
+            new_dict[key] = value.to(device)
+        else:
+            new_dict[key] = value
+    return new_dict
+
+
+pred_energies = []
+true_energies = []
+pred_forces = []
+true_forces = []
+
+for index, batch in enumerate(val_loader):
+    # make sure we don't contaminate
+    task.zero_grad(True)
+    batch = to(batch, task.device)
+    outputs = task(batch)
+    energies = outputs["energy"].detach().cpu().numpy()
+    forces = outputs["force"].detach().cpu().numpy()
+    pred_energies.append(energies)
+    pred_forces.append(forces)
+    true_energies.append(batch["targets"]["energy"].cpu().numpy())
+    true_forces.append(batch["targets"]["force"].cpu().numpy())
+
+infer_art = wandb.Artifact(name="mace-uip-validation", type="result")
+
+for array, name in zip(
+    [pred_energies, pred_forces, true_energies, true_forces],
+    ["pred_energies", "pred_forces", "true_energies", "true_forces"],
+):
+    output_path = artifact_dir.joinpath(name).with_suffix(".npy")
+    array = np.vstack(array)
+    np.save(output_path, array)
+    infer_art.add_file(local_path=output_path, name=name)
+
+run.log_artifact(infer_art)
